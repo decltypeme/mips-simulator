0b614143494f29e03d4543d5738594bab5c0a6d1
Islam Faisal <islamfm@aucegypt.edu>
2015-12-05 00:17:25 +0200

    Addi.cpp file added

---
+++ mips/mips/Addi.cpp
@@ -0,0 +1,26 @@
+#include "Addi.h"
+
+
+
+Addi::Addi()
+{
+}
+
+
+Addi::~Addi()
+{
+}
+
+void Addi::execute()
+{
+	writeData = readData[0] + immediate;
+}
+
+void Addi::memory()
+{
+}
+
+void Addi::writeBack()
+{
+	writeRegister(destination, writeData);
+}
