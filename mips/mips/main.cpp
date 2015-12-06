1b7267da3f79020ff067281a42e5c99e6a975163
Islam Faisal <islamfm@aucegypt.edu>
2015-12-06 04:43:09 +0200

    Added form, first successful build

--- mips/mips/main.cpp
+++ mips/mips/main.cpp
@@ -1,9 +1,28 @@
 
 #include "parser.h"
+#include "Simulator.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+
+void uiInitialize()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+}
+
+[STAThread]
 int main()
 {
 	initialize();
+	uiInitialize();
+
+	mips::Simulator mainSimulatorForm;
+	Application::Run(%mainSimulatorForm);
+
 
 	while (true)
 	{
