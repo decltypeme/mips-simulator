9eb7afdc481c2da59f6d29ed68fe72934c31b1c4
Hussam Ashraf El-Araby <hussam.a@aucegypt.edu>
2015-12-11 22:27:28 +0200

    added hazard msgs

--- mips/mips/hazards.h
+++ mips/mips/hazards.h
@@ -1,6 +1,15 @@
 #pragma once
+#include <string>
 
 extern int z;
-extern int flushHappened;
 void hazardDetection();
 void dealWithForwarding(int value);
+
+struct hazardMsg 
+{
+	int value;
+	std::string hazard;
+	hazardMsg() : hazardMsg(0, "No Hazard") {}
+	hazardMsg(int i, string j) : value(i) , hazard(j) {}
+};
+extern hazardMsg hazardMsgs[14];
